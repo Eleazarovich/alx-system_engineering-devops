9a070bbd4913b58546a7a9c71ef9c9aecbb5e9b1
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-05-23 12:05:03 +0200

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,13 +15,13 @@ def main():
     user = requests.get(f'{url}users/{user_id}').json()
     todos = requests.get(f'{url}todos', params={"userId": user_id}).json()
     username = user.get('username')
-    
+
     with open(f"{user_id}.csv", "w", newline="") as csv_file:
         data = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
         for title in todos:
-            data.writerow([user_id, username, title.get("completed"),\
-                    title.get("title")])
+            data.writerow([user_id, username, title.get("completed"),
+                           title.get("title")])
 
 
 if __name__ == "__main__":
