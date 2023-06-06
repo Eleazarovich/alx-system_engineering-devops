fa67211444c9b6efb2c7163e2e9b64c24593330d
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-06-06 20:27:17 +0200

    minor fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,9 @@ def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
     headers = {'User-agent': 'Eleazarovich'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        return response.json().get('data', {}).get('subscribers', 0)
+        return response.json().get('data').get('subscribers')
     else:
         return 0
