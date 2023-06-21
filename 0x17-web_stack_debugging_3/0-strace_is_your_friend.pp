fcc3d2f1bee9cc31d605ebf2c9a70728fefe214e
Eleazarovich <96009758+Eleazarovich@users.noreply.github.com>
2023-06-21 16:05:19 +0200

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,2 +0,0 @@
-# puppet script to fix error 500 in Apache server
-exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
--- 0x17-web_stack_debugging_3/README.md
+++
@@ -1,8 +0,0 @@
-# 0x17-web_stack_debugging_3
-
----
-* Web stack debugging #3
----
-## Tasks
-* Mandatory
-- [x] Task 0
